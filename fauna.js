7dd390d519c0fe51887e91af186a1ed83914fb55
david-hogg <theboydavid+github@gmail.com>
2022-12-17 12:07:05 +0000

    added fauna connection
    added fauna connection

---
+++ fauna.js
@@ -0,0 +1,20 @@
+const faunadb = require('faunadb'); // Make sure you have the FaunaDB Node.js client library installed
+
+// Read the FaunaDB secret from an environment variable
+const faunaSecret = process.env.FAUNADB_SECRET;
+
+const client = new faunadb.Client({ secret: faunaSecret });
+
+module.exports = async () => {
+  // Run a query to retrieve all documents from the 'records' collection
+  const response = await client.query(
+    q.Paginate(
+      q.Match(
+        q.Ref('indexes/all_records')
+      )
+    )
+  );
+
+  // Return the array of documents
+  return response.data;
+};
