0689d947106ff0869e869a77b16675d5a0962a99
david-hogg <theboydavid+github@gmail.com>
2022-12-17 12:26:53 +0000

    faunadata

--- _data/fauna.js
+++ _data/fauna.js
@@ -4,6 +4,8 @@ const faunadb = require('faunadb'); // Make sure you have the FaunaDB Node.js cl
 const faunaSecret = process.env.FAUNADB_SECRET;
 
 const client = new faunadb.Client({ secret: faunaSecret });
+const q = faunadb.query; // Import the 'query' object from the faunadb module
+
 
 module.exports = async () => {
   // Run a query to retrieve all documents from the 'records' collection
